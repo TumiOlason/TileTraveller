5030f1bec180a3b9916a1a9b5e62e3382b47b8cb
Sturla Erlendsson <sturla19@ru.is>
2020-09-17 14:53:42 +0000

    Sturla commit

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,90 +1,69 @@
+def position_changer(number,position):
+    number = number.lower()
+
+    if number == 'n':
+        position += 1
+    elif number == 'e':
+        position += 10
+    elif number == 's':
+        position -= 1
+    elif number == 'w':
+        position -= 10
+    else:
+        print("Not a valid direction!")
+        return position
+    
+    return position
+
+
+
 position = 11
 n = "(N)orth"
 e = "(E)ast"
 w = "(W)est"
 s = "(S)outh"
+
+
 while position != 31 :
     if position == 11:
         print("You can travel: {}.".format(n))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "n":
-            position += 1
-        else:
-            print("Not a valid direction!")
+        position = position_changer(choice,position)
 
     elif position == 12:
         print("You can travel: {} or {} or {}.".format(n,e,s))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "n":
-            position += 1
-        elif choice == "s":
-            position -= 1
-        elif choice == "e":
-            position += 10
-        else:
-            print("Not a valid direction!")
+        position = position_changer(choice,position)
+
     elif position == 13:
         print("You can travel: {} or {}.".format(e,s))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "s":
-            position -= 1
-        elif choice == "e":
-            position += 10
-        else:
-            print("Not a valid direction!")
+        position = position_changer(choice,position)
 
     elif position == 21:
         print("You can travel: {}.".format(n))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "n":
-            position += 1
-        else:
-            print("Not a valid direction!")
+        position = position_changer(choice,position)
 
     elif position == 22:
         print("You can travel: {} or {}.".format(s,w))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "w":
-            position -= 10
-        elif choice == "s":
-            position += 1
-        else:
-            print("Not a valid direction!")
+        position = position_changer(choice,position)
 
     elif position == 23:
         print("You can travel: {} or {}.".format(e,w))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "w":
-            position -= 10
-        elif choice == "e":
-            position += 10
-        else:
-            print("Not a valid direction!")
+        position = position_changer(choice,position)
+
     elif position == 33:
         print("You can travel: {} or {}.".format(s,w))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "s":
-            position -= 1
-        elif choice == "w":
-            position -= 10
-        else:
-            print("Not a valid direction!")
+
+        position = position_changer(choice,position)
 
     elif position == 32:
         print("You can travel: {} or {}.".format(n,s))
         choice = input("Direction: ")
-        choice = choice.lower()
-        if choice == "n":
-            position += 1
-        elif choice == "s":
-            position -= 1
-        else:
-            print("Not a valid direction!")
+        position = position_changer(choice,position)
+
 print("Victory!")
