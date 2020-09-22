bd74ad5e67d5839c47bc4edf50a6a9c0f9f9d4f1
tumiola <tumi19@ru.is>
2020-09-22 08:47:08 +0000

    last commit

--- TileTraveller.py
+++ TileTraveller.py
@@ -34,6 +34,7 @@ def position_allowed(position):
         return "ns"
     elif position ==33:
         return "sw"
+
 position = 11
 n = "(N)orth"
 e = "(E)ast"
@@ -44,43 +45,28 @@ s = "(S)outh"
 while position != 31 :
     if position == 11:
         print("You can travel: {}.".format(n))
-        choice = input("Direction: ")
-        position = position_changer(choice,position,position_allowed(position))
 
     elif position == 12:
         print("You can travel: {} or {} or {}.".format(n,e,s))
-        choice = input("Direction: ")
-        position = position_changer(choice,position,position_allowed(position))
 
     elif position == 13:
         print("You can travel: {} or {}.".format(e,s))
-        choice = input("Direction: ")
-        position = position_changer(choice,position,position_allowed(position))
 
     elif position == 21:
         print("You can travel: {}.".format(n))
-        choice = input("Direction: ")
-        position = position_changer(choice,position,position_allowed(position))
 
     elif position == 22:
         print("You can travel: {} or {}.".format(s,w))
-        choice = input("Direction: ")
-        position = position_changer(choice,position,position_allowed(position))
 
     elif position == 23:
         print("You can travel: {} or {}.".format(e,w))
-        choice = input("Direction: ")
-        position = position_changer(choice,position,position_allowed(position))
 
     elif position == 33:
         print("You can travel: {} or {}.".format(s,w))
-        choice = input("Direction: ")
-
-        position = position_changer(choice,position,position_allowed(position))
 
     elif position == 32:
         print("You can travel: {} or {}.".format(n,s))
-        choice = input("Direction: ")
-        position = position_changer(choice,position,position_allowed(position))
 
+    choice = input("Direction: ")
+    position = position_changer(choice, position, position_allowed(position))
 print("Victory!")
